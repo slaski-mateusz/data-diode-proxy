e9be48193ce82d154e76e7c287064715bf455902
Mateusz Ślaski <slaski.mateusz@gmail.com>
2023-01-31 23:49:07 +0100

    Work in progress. Many changes not tested yet.

--- agents/tx-agent/txagent.go
+++ agents/tx-agent/txagent.go
@@ -1,64 +1,141 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
+	"path"
+	"path/filepath"
 	"regexp"
+	"sync"
+	"syscall"
 	"time"
 
 	"gopkg.in/yaml.v3"
 )
 
+// few global variables
+
+var (
+	configuration configDef
+	pkgTtl        int64
+	cycleDuration time.Duration
+)
+
+// Configuration file types
+
 type ttlDef struct {
-	Seconds int `yaml:"seconds"`
-	Minutes int `yaml:"minutes"`
-	Hours   int `yaml:"hours"`
+	Seconds int64 `yaml:"seconds"`
+	Minutes int64 `yaml:"minutes"`
+	Hours   int64 `yaml:"hours"`
 }
 
 type cycleDef struct {
-	Seconds int `yaml:"seconds"`
+	Seconds int64 `yaml:"seconds"`
+}
+
+type packetSizeDef struct {
+	Bytes int `yaml:"bytes"`
 }
 
 type configDef struct {
-	Workdir     string   `yaml:"workdir"`
-	FilePattern string   `yaml:"fpattern"`
-	Cycle       cycleDef `yaml:"cycle"`
-	Ttl         ttlDef   `yaml:"ttl"`
+	Workdir     string        `yaml:"workdir"`
+	FilePattern string        `yaml:"fpattern"`
+	Cycle       cycleDef      `yaml:"cycle"`
+	Ttl         ttlDef        `yaml:"ttl"`
+	PacketSize  packetSizeDef `yaml:"packet_size"`
+}
+
+// Buffer types
+
+type validTill int64
+
+type packageId int64
+
+type transmittedFileName string
+
+type bufferDef struct {
+	sync.Mutex
+	content map[validTill]map[transmittedFileName]map[packageId][]byte
+}
+
+// Functions
+
+func createDoneDirectory() {
+	doneDirPath := path.Join(configuration.Workdir, "done")
+	if _, errSt := os.Stat(doneDirPath); errors.Is(errSt, os.ErrNotExist) {
+		errMk := os.Mkdir(doneDirPath, os.ModePerm)
+		if errMk != nil {
+			log.Fatalf(
+				"Can not create 'done' directory in working directory %v due to problem: %v",
+				configuration.Workdir,
+				errMk,
+			)
+		}
+	}
 }
 
 func (c *configDef) Load(confFileName string) {
 	confFile, errRead := ioutil.ReadFile(confFileName)
-	fmt.Println(errRead)
 	if errRead != nil {
 		log.Fatalf("Problem with reading config file '%s': %v", confFileName, errRead)
 	}
-	fmt.Print(string(confFile))
 	errYaml := yaml.Unmarshal(confFile, c)
-	fmt.Println(errYaml)
-	fmt.Println(&c)
 	if errYaml != nil {
 		log.Fatalf("Problem with config yaml un-marshaling: %v", errYaml)
 	}
 }
 
-func main() {
-	fmt.Println(os.Executable())
-	var cf configDef
-	cf.Load("txagent.yaml")
-	fmt.Printf("%+v\n", cf)
-	pkgTtl := cf.Ttl.Seconds + 60*cf.Ttl.Minutes + 3600*cf.Ttl.Hours
-	cycleDuration := time.Duration(
-		cf.Cycle.Seconds * int(time.Second),
-	)
-	fmt.Println(cf.Workdir, pkgTtl)
+func configFileName() string {
+	executablePath, errExe := os.Executable()
+	if errExe != nil {
+		log.Fatalf("Problem with getting my executable name: %v", errExe)
+	}
+	_, executableName := path.Split(executablePath)
+	return executableName
+}
+
+func putFileContentToBuffer(fileName string, fileToRead *os.File, buffer *bufferDef) {
+	bytesRead := make([]byte, configuration.PacketSize.Bytes)
+	var fileContent []byte
+	buffer.Lock()
+	defer buffer.Unlock()
+	var pkgId packageId = 1
 	for {
-		files, errRd := ioutil.ReadDir(cf.Workdir)
+		var amountRead int
+		amountRead, _ = fileToRead.Read(bytesRead)
+		fmt.Print(amountRead, " ")
+		vt := validTill(time.Now().Unix() + int64(pkgTtl))
+		buffer.content[vt][transmittedFileName(fileName)][pkgId] = bytesRead
+		pkgId++
+		if amountRead == 0 {
+			break
+		}
+	}
+	fmt.Println(string(fileContent))
+}
+
+func removeOutdatedPackagesFromBuffer(buffer *bufferDef) {
+	nowSeconds := time.Now().Unix()
+	buffer.Lock()
+	defer buffer.Unlock()
+	for ts := range buffer.content {
+		if ts < validTill(nowSeconds-pkgTtl) {
+			delete(buffer.content, ts)
+		}
+	}
+}
+
+func checkForNewFilesToTransmit(buffer *bufferDef) {
+	for {
+		files, errRd := ioutil.ReadDir(configuration.Workdir)
 		if errRd != nil {
 			log.Fatalf(
 				"Can not list working directory '%s'. Error: %v",
-				cf.Workdir,
+				configuration.Workdir,
 				errRd,
 			)
 		}
@@ -67,19 +144,53 @@ func main() {
 		fmt.Println(time.Now())
 		fmt.Println(timeToCheck)
 		for _, f := range files {
-			matchPattern, _ := regexp.MatchString(
-				cf.FilePattern,
-				f.Name(),
-			)
-			if f.Mode().IsRegular() {
-				if matchPattern {
-					if f.ModTime().After(timeToCheck) {
-						fmt.Println(f.Name(), f.ModTime(), " ")
-					}
-				}
+			fileMatchPattern, _ := regexp.MatchString(configuration.FilePattern, f.Name())
+			if !fileMatchPattern {
+				continue
 			}
+			fileIsRegular := f.Mode().IsRegular()
+			if !fileIsRegular {
+				continue
+			}
+			fileIsRecentlyModified := f.ModTime().After(timeToCheck)
+			if !fileIsRecentlyModified {
+				continue
+			}
+			fmt.Println(f.Name(), f.ModTime(), " ")
+			filePath := filepath.Join(configuration.Workdir, f.Name())
+			fileToRead, errOpen := os.Open(filePath)
+			if errOpen != nil {
+				fmt.Printf("can not read file %v %v\n", f.Name(), errOpen)
+			} else {
+				putFileContentToBuffer(f.Name(), fileToRead, buffer)
+			}
+
 		}
 		fmt.Println()
 		time.Sleep(cycleDuration)
 	}
+}
+
+// main
+
+func main() {
+
+	cancelChan := make(chan os.Signal, 1)
+	signal.Notify(
+		cancelChan,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+	)
+	configuration.Load(configFileName() + ".yaml")
+	createDoneDirectory()
+	pkgTtl = configuration.Ttl.Seconds + 60*configuration.Ttl.Minutes + 3600*configuration.Ttl.Hours
+	cycleDuration = time.Duration(
+		configuration.Cycle.Seconds * int64(time.Second),
+	)
+	var buffer bufferDef
+	// TODO: Load buffer from file
+	go checkForNewFilesToTransmit(&buffer)
+	sig := <-cancelChan
+	log.Printf("Caught signal %v", sig)
+	// TODO: Save buffer to file
 }
